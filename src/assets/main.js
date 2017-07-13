9c930fbe63354084164c8a2a7bf4cbfff95e5857
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2017-07-13 17:47:21 +0530

    Done following things to fetch the badges from codeschool made ajax call, displayed badges, displayed course titles

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,19 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/2292865.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      for (var i = 0; i < response.courses.completed.length; i++) {
+        var courseDiv = "<div class='course'><h3>"+
+        response.courses.completed[i].title  +"</h3>"+
+        "<img src='"+ response.courses.completed[i].badge +"'>"+
+        "<a href='"+response.courses.completed[i].url+
+        "' class='btn btn-primary' target='_blank'>See Course</a>"+
+        "</div>";
+        $('#badges').append(courseDiv);
+      }
+    }
+  });
 
 });
